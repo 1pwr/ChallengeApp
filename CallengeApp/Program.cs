c1dd1ae3c64c659411346418cdd6a72747017937
1pwr <jeszurun@gmail.com>
2024-01-28 15:00:11 +0100

    Praca z dnia 4

--- CallengeApp/Program.cs
+++ CallengeApp/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Witam wszystkich!");
+﻿// praca dzień 4
+// płeć "m" - mężczyzna "f" - Kobieta lub nie zdefiniowana
+const int age18 = 18, age30 = 30;
+const string nameEwa = "Ewa";
+
+var sex = "f";
+var age = 27;
+var name = "Ewa";
+
+if (name == nameEwa && age == age30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "f" && age < age30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == "m" && age < age18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
